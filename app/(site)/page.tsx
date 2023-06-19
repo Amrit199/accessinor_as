5d1dec8d46798b56e988540e2178af1d7bfc882e
Amrit <tamangamrit37@gmail.com>
2023-06-19 16:52:19 +0200

    Update page.tsx
    update

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,10 +14,10 @@ export default async function Home() {
         <div className="w-full px-8 flex flex-col items-center justify-center gap-4 text-center">
           <h1 className=" text-5xl font-bold">Find out your <span className=" font-normal text-indigo-600">Website Accessibility</span></h1>
           <p className=" text-md">
-            By assessing your website's level of conformance with globally
-            recognized digital accessibility standards, the Website
-            Accessibility Health Score ensures barrier-free access to technology
-            for all users, including those with disabilities
+            By assessing your websites level of conformance with globally
+            recognized digital accessibility standards the Website
+            Accessibility Health Score ensures barrier free access to technology
+            for all users including those with disabilities
           </p>
         </div>
         <div className="w-full flex justify-center">
