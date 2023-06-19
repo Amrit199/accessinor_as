e5b959dabf19c977b9d2c138b20f8079db78dabe
Amrit <tamangamrit37@gmail.com>
2023-06-19 16:46:31 +0200

    Update page.tsx
    update

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,24 @@ export default async function Home() {
   return (
     <div className="bg-white text-black pt-20">
       {/* hero section */}
-      <Demo />
+      <div className="w-full h-full flex items-center text-black py-40 px-5">
+        <div className="w-full px-8 flex flex-col items-center justify-center gap-4 text-center">
+          <h1 className=" text-5xl font-bold">Find out your <span className=" font-normal text-indigo-600">Website Accessibility</span></h1>
+          <p className=" text-md">
+            By assessing your website's level of conformance with globally
+            recognized digital accessibility standards, the Website
+            Accessibility Health Score ensures barrier-free access to technology
+            for all users, including those with disabilities
+          </p>
+        </div>
+        <div className="w-full flex justify-center">
+          <Image
+            src={responsive}
+            alt="accessibility checked image"
+            className=" w-96 h-96"
+          />
+        </div>
+      </div>
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
