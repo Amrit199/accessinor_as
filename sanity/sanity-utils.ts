ad3af8898faac3906ca568a08eda4e53f9b541dc
Amrit <tamangamrit37@gmail.com>
2023-06-21 16:34:37 +0200

    update
    update

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -2,6 +2,7 @@ import { Page } from "@/types/Page";
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import config from "./config/client-config";
+import { Post } from "@/types/Post";
 
 export async function getProjets(): Promise<Project[]> {
     const client = createClient({
@@ -31,6 +32,21 @@ export async function getPages(): Promise<Page[]> {
             "slug": slug.current,
             "image": image.asset->url,
             title,
+            content
+        }`
+    )
+}
+
+export async function getPosts(): Promise<Post[]> {
+    return createClient(config).fetch(
+        groq`*[_type == "post"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            title,
+            tags,
+            "image": image.asset->url,
             content
         }`
     )
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,7 @@
 import page from "./pages-schema";
+import post from "./posts-schema";
 import project from "./project-schema";
 
-const schemas = [project, page]
+const schemas = [project, page, post]
 
 export default schemas
