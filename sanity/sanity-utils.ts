e319005c16df416c49c4f4087ef6b987449ca9e7
Amrit <tamangamrit37@gmail.com>
2023-10-16 14:15:17 +0200

    update
    update

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,36 +1,6 @@
-import { Page } from "@/types/Page";
-import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import config from "./config/client-config";
 import { Post } from "@/types/Post";
-
-export async function getProjets(): Promise<Project[]> {
-    return createClient(config).fetch(
-      groq`*[_type == "project"]{
-        _id,
-        _createdAt,
-        name,
-        "slug": slug.current,
-        "image": image.asset->url,
-        url,
-        content
-      }`  
-    )
-}
-
-export async function getPages(): Promise<Page[]> {
-    return createClient(config).fetch(
-        groq`*[_type == "page"]{
-            _id,
-            _createdAt,
-            name,
-            "slug": slug.current,
-            "image": image.asset->url,
-            title,
-            content
-        }`
-    )
-}
 
 export async function getPosts(): Promise<Post[]> {
     return createClient(config).fetch(
